07bd946ffb089eb1a7ea3dfe9500c00b7ceda77b
Urs Braun <urs@ursb.de>
2021-11-07 16:22:23 +0100

    exchange

--- index.php
+++ index.php
@@ -4,36 +4,32 @@
 <head>
     <title>Urs Webseite</title>
 
+    <meta charset="utf-8">
     <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
     <meta name="description" content="" />
-    <meta name="author" content="" />
+    <meta name="author" content="Urs-Benedict Braun" />
     <meta name="keywords" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap core CSS -->
     <link href="bootstrap/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
     <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+    
     <!-- Eigene Einstellunfen -->
     <style>
-    body {
-      padding-bottom: 20px;
-    }
-    
-    .navbar {
-      margin-bottom: 20px;
-    }
-	.b-example-divider {
-	  height: 3rem;
-	  background-color: rgba(0, 0, 0, .1);
-	  border: solid rgba(0, 0, 0, .15);
-	  border-width: 1px 0;
-	  box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-	}
-
-	.bi {
-	  vertical-align: -.125em;
-	  fill: currentColor;
-	}
+      body {
+        padding-bottom: 20px;
+      }
+      
+      .navbar {
+        margin-bottom: 20px;
+      }
+      .bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
     </style>
+    
     <!-- Favicon -->
     <link href="img/ico/favicon.ico" type="image/x-icon" rel="shortcut icon" />
 </head>
@@ -42,7 +38,7 @@
     $site = $_GET['site'];
    ?>
 <!-- Ende -->
-<body>
+<body class="bg-dark text-white">
 <!-- Navigationsleiste -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Navigation">
     <div class="container">
@@ -54,26 +50,36 @@
       <div class="collapse navbar-collapse" id="navbarU">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">               
           
-          <!-- Zweite -->
-          <?php if ($site == 'about'):  ?>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/?site=about">&Uuml;ber mich</a>
-                </li>
-            <?php else: ?>
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="/?site=about">&Uuml;ber mich</a>
-                </li>
-            <?php endif ?>
+          <!-- Erste -->
+          <li class="nav-item">
+                    <a class="<?php 
+                          if ($site == 'about'){
+                            echo "nav-link active";
+                          }else{
+                            echo "nav-link ";
+                          }
+                    ?>" aria-current="page" href="/?site=about">&Uuml;ber mich</a>
+          </li>
+
+          <!-- Zweiter Eintrag -->
+          <li class="nav-item">
+                    <a class="<?php 
+                          if ($site == 'links'){
+                            echo "nav-link active";
+                          }else{
+                            echo "nav-link";
+                          }
+                    ?>" aria-current="page" href="/?site=links">Links</a>
+          </li>  
                 
-                
-          <!-- Dropdown Menü -->
+          <!-- Dropdown MenÃ¼ 
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="dropdownU" data-bs-toggle="dropdown" aria-expanded="false">Rechtliche Angaben</a>
             <ul class="dropdown-menu" aria-labelledby="dropdownU">
               <li><a class="dropdown-item" href="?site=imprint">Impressum</a></li>
               <li><a class="dropdown-item" href="?site=policy">Datenschutz</a></li>
             </ul>
-          </li>
+          </li> End of Dropdown Menu -->
         </ul>
         <form>
           <input class="form-control" type="text" placeholder="Search" aria-label="Search">
@@ -94,6 +100,8 @@
        }
        else if ($site == 'policy'){
            include 'sites/policy.php';
+       }else if ($site == 'links'){
+          include 'sites/links.php';
        }
 
     }else{
@@ -113,7 +121,7 @@
 
     <ul class="nav col-md-4 justify-content-end">
       <li class="nav-item"><a href="?site=imprint" class="nav-link px-2 text-muted">Impressum</a></li>
-      <li class="nav-item"><a href="?site=policy" class="nav-link px-2 text-muted">Datenschutz</a></li>
+      <!--<li class="nav-item"><a href="?site=policy" class="nav-link px-2 text-muted">Datenschutz</a></li>-->
     </ul>
   </footer>
 </div>  
